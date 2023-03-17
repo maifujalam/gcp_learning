976ef56c949444c0c69ef5cbb3390732597b3ce9
alam <sk.maifujalam@gmail.com>
2023-03-17 19:16:25 +0530

    Terraform ip ssh output1.0

--- resources_using_modules/compute/instances/ansible-vms/outputs.tf
+++ resources_using_modules/compute/instances/ansible-vms/outputs.tf
@@ -3,11 +3,11 @@ output "private_ip" {
 }
 output "public_ip" {
   #value = length(module.bastion-vm[*].public-ips) > 0 ? module.bastion-vm[*].public-ips : null
-  value = length(module.bastion-vm[*].public-ips) > 0 ? flatten(module.bastion-vm[*].public-ips): null
+  value = var.external_ip ? flatten(module.bastion-vm[*].public-ips): null
 }
 
 output "ssh" {
-  value= length(module.bastion-vm[*].public-ips) > 0 ?  [ for i in flatten(module.bastion-vm[*].public-ips): "ssh -i ~/.ssh/gcp/gcp -o ServerAliveInterval=5 ${var.ssh_user}@${i}"] : null
+  value= var.external_ip ?  [ for i in flatten(module.bastion-vm[*].public-ips): "ssh -i ~/.ssh/gcp/gcp -o ServerAliveInterval=5 ${var.ssh_user}@${i}"] : null
 }
 #output "ssh2" {
 #  value= "ssh -i ~/.ssh/gcp/gcp -o ServerAliveInterval=5 alam@${(module.bastion-vm[0].private-ips)}"
