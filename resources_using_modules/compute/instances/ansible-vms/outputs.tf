4584f64ff78cc1ff20c4b59105a2a70ad4dd072f
alam <sk.maifujalam@gmail.com>
2023-03-18 10:22:29 +0530

    Terraform ip ssh output2.0

--- resources_using_modules/compute/instances/ansible-vms/outputs.tf
+++ resources_using_modules/compute/instances/ansible-vms/outputs.tf
@@ -4,7 +4,6 @@ output "private_ip" {
 output "public_ip" {
   value = var.external_ip ? zipmap(flatten(module.bastion-vm[*].names),flatten(module.bastion-vm[*].public-ips)): null
 }
-
 output "ssh" {
   value= var.external_ip ?  zipmap(flatten(module.bastion-vm[*].names),[ for i in flatten(module.bastion-vm[*].public-ips): "ssh -i ~/.ssh/gcp/gcp -o ServerAliveInterval=5 ${var.ssh_user}@${i}"]) : null
 }
--- resources_using_modules/compute/instances/ansible-vms/variables.tf
+++ resources_using_modules/compute/instances/ansible-vms/variables.tf
@@ -17,10 +17,12 @@ variable "machine-type" {
   description = "VM Machine type"
   type = map(any)
   default = {
-    "c2m1" = "e2-micro"
-    "c2m2" = "e2-small"
+    "c1m1free" = "e2-micro"
+    "c1m2" = "e2-small"
     "c2m4" = "e2-medium"
     "c2m8" = "e2-standard-2"
+    "C3c4m8" = "c3-highcpu-4"
+    "C3c8m16" = "c3-highcpu-8"
   }
 }
 variable "name" {
